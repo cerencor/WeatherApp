0ca2ed3983d7042fdcaea76874cb0a4b1b32d445
cerencor <62300722+cerencor@users.noreply.github.com>
2024-08-06 16:04:15 +0300

    changed api link

--- screens/WeatherScreen.js
+++ screens/WeatherScreen.js
@@ -22,10 +22,10 @@ const WeatherScreen = ({ route }) => {
 
   const fetchWeather = async (cityName) => {
     const weatherResponse = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+      `https://api.openweathermap.org/data/3.0/weather?q=${cityName}&appid=${API_KEY}&units=metric`
     );
     //const weatherResponse = await axios.get(
-      //`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
+      //`https://api.openweathermap.org/data/3.0/weather?q=${cityName}&appid=${API_KEY}&units=metric`
     //);
     const weatherJson = await weatherResponse.json();
     console.log("Weather response:", weatherJson);
